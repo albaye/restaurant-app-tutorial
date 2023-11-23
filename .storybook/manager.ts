f7c06ae694cc5c1f7860882a3143f71f8295f1f6
albaye <alba.ye99@gmail.com>
2023-11-23 13:49:16 +0000

    Storybook theming

---
+++ .storybook/manager.ts
@@ -0,0 +1,19 @@
+import { addons } from "@storybook/manager-api";
+import { create } from "@storybook/theming";
+
+addons.setConfig({
+  theme: create({
+    base: "light",
+
+    brandTitle: "A design system for life",
+    brandUrl: "https://chan.dev",
+    // brandImage: "",
+    brandTarget: "_self",
+
+    colorSecondary: "#ffcc00",
+    appBg: "#f0f0f0",
+    appBorderColor: "#ccc",
+    textColor: "#333",
+    barTextColor: "#666"
+  })
+})
